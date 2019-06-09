ae21447d5490bc7d9c0aae90a0a3b0cb627bf92e
AlessandroBuono <alessandro.buono91@gmail.com>
2019-06-09 18:34:05 +0200

    Progetto vuoto

---
+++ ProgettoSIW2019/src/main/java/it/uniroma3/siw/ProgettoSIW2019/ProgettoSiw2019Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.ProgettoSIW2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSiw2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSiw2019Application.class, args);
+	}
+
+}
